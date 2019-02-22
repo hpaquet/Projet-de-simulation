673b69487c78d3a3f15f08322c17e84655b1159b
hpaquet <paquet.hugo@gmail.com>
2019-02-22 16:13:06 -0500

    maj

--- MD_simulation.m
+++ MD_simulation.m
@@ -1,29 +1,42 @@
-function [  ] = MD_simulation(protein, duration)
+function protein = MD_simulation(protein, type, duration, v0)
 
-global V;
+global V epsi;
 
-N = length(protein);
+N = size(protein,2)/2;
+
+h=plot(0,0,'MarkerSize',100,'Marker','.');
+
+set(gca,'nextplot','replacechildren');
 
 
 %% première itération
 
 for n = 1:N % ensemble des AA
-    F = Force();
-    protein(2*n-1,2) = 2*V(10)/V(4)*F(1) + 2*V(10)*sqrt(2*V(0.2))*V(7) + protein(2*n-1,1) - V(10)*vo;
-    protein(2*n,2) = 2*V(10)/V(4)*F(2) + 2*V(10)*sqrt(2*V(0.2))*V(7) + protein(2*n-1,1) - V(10)*vo;
+    [Fx, Fy] = sum_force( protein, type, n, V, 1, epsi);
+    
+    protein(2,2*n-1) = 2*V(9)/V(4)*Fx + 2*V(9)*sqrt(2*V(5))*V(7) + protein(1,2*n-1) - V(9)*v0(n);
+    protein(2,2*n) = 2*V(9)/V(4)*Fy + 2*V(9)*sqrt(2*V(5))*V(7) + protein(1,2*n) - V(9)*v0(n);
 end
 
 %% boucle principal
 
-for t = 2:duration % variation dans le temps
-    
+for t = 3:duration % variation dans le temps
     
     for n = 1:N % ensemble des AA
         
+        [Fx, Fy] = sum_force( protein, type, n, V, t-1, epsi);
+        
+        protein(t,2*n-1) = 2*V(9)/V(4)*Fx + 2*V(9)*sqrt(2*V(5))*V(7) + protein(t-1,2*n-1);
+        protein(t,2*n) = 2*V(9)/V(4)*Fy + 2*V(9)*sqrt(2*V(5))*V(7) + protein(t-1,2*n);
+            
         
     end
+    
+    set(h,'XData',[protein(t,1) protein(t,3)],'YData',[protein(t,2) protein(t,4)]);
+    drawnow;
+    pause(0.1)
+    
 end
-
 
 end
 
--- sum_force.m
+++ sum_force.m
@@ -1,43 +1,43 @@
 function [ Fx, Fy ] = sum_force( protein, type, i, V,t,e)
 
-    l = length(protein);
+    l = size(protein,2);
     
     Fx = 0;
     Fy = 0;
 
-    for n = 1:l
+    for n = 1:l/2
         
         if n ==i
             continue
             
         elseif n==i-1 || n== i+1
             
-            dx = protein(2*n-1,t) - protein(2*i-1,t);
-            dy = protein(2*n,t) - protein(2*i,t);
+            dx = protein(t,2*n-1) - protein(t,2*i-1);
+            dy = protein(t,2*n) - protein(t,2*i);
             
             r = sqrt(dx^2+dy^2);
             
             theta = atan(dy/dx);
             
-            k = LenardJones(V(6),r,e(type(i),type(n)),2)*10;
+            k = 1;%LenardJones(V(6),r,e(type(i),type(n)),2)*10;
             
             F = -0.5*k*r^2;
             
             Fx = Fx + F*cos(theta);
             Fy = Fy + F*sin(theta);
             
-        else
-            dx = protein(2*n-1,t) - protein(2*i-1,t);
-            dy = protein(2*n,t) - protein(2*i,t);
-            
-            r = sqrt(dx^2+dy^2);
-            
-            theta = atan(dy/dx);
-            
-            F = -LenardJones(V(6),r,e(type(i),type(n)),1);
-            
-            Fx = Fx + F*cos(theta);
-            Fy = Fy + F*sin(theta);
+%         else
+%             dx = protein(t,2*n-1) - protein(t,2*i-1);
+%             dy = protein(t,2*n) - protein(t,2*i);
+%             
+%             r = sqrt(dx^2+dy^2);
+%             
+%             theta = atan(dy/dx);
+%             
+%             F = -LenardJones(V(6),r,e(type(i),type(n)),1);
+%             
+%             Fx = Fx + F*cos(theta);
+%             Fy = Fy + F*sin(theta);
             
         end
         
